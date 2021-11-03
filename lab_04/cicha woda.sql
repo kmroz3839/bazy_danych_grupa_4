1d926bed21b0176bc5da29ea29054f7851de6735
counter185 <33550839+counter185@users.noreply.github.com>
2021-11-03 12:44:11 +0100

    dodanie lab_04 zanim się znowu laptop rozładuje

---
+++ lab_04/cicha woda.sql
@@ -0,0 +1,38 @@
+--zadanie 1
+
+SELECT * FROM postac WHERE rodzaj = "wiking" AND nazwa != "Bjorn" ORDER BY wiek DESC;
+
+SET foreign_key_checks = 0;
+
+ALTER TABLE postac DROP FOREIGN KEY statek;
+
+ALTER TABLE postac MODIFY id_postaci INT UNSIGNED;
+
+ALTER TABLE postac DROP PRIMARY KEY;
+
+
+
+--zadanie 2
+
+ALTER TABLE postac ADD pesel CHAR(11);
+
+ALTER TABLE postac ADD PRIMARY KEY (pesel);
+
+ALTER TABLE postac MODIFY COLUMN rodzaj ENUM('wiking', 'ptak', 'kobieta', 'syrena')
+
+INSERT INTO postac(id_postaci, nazwa, rodzaj) VALUES
+	(10, "Gertruda Nieszczera", 'syrena');
+
+
+
+--zadanie 3
+
+UPDATE postac SET statek = "niszczyciel" WHERE nazwa LIKE "%a%";
+
+UPDATE statek SET max_ladownosc = max_ladownosc*0.7 WHERE YEAR(data_wodowania) > 1900 AND YEAR(data_wodowania) < 2001;
+
+ALTER TABLE postac ADD CHECK (wiek <= 1000)
+
+
+
+--zadanie 4
